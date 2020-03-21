2c720ad4880d0e0f30f40df2e657ebe3268e5a7d
alsdede <andreluizsilveira.88@gmail.com>
2020-03-21 16:46:39 -0300

    Initial Config Redux to Auth users

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
 import { ReactComponent as Logo } from '~/assets/logo.svg';
-// import { Container } from './styles';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -12,7 +15,9 @@ const schema = Yup.object().shape({
   password: Yup.string().required('A senha é obrigatória'),
 });
 export default function SignIn() {
-  function handleSubmit(data) {
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
     console.tron.log(data);
   }
 
